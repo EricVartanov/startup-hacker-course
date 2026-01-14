58993d1b37b5a61eb3a4c5ed993ae30c2d48bd19
Eric <eric@Erics-MacBook-Air.local>
2026-01-14 14:27:56 +0300

    copy from day2 to day3

---
+++ day_3/src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="app">
+    <!--<FontAwesomeIcon icon="star" />-->
+    <div class="container">
+      <Card v-for="book in books" :key="book.title" :book="book" />
+    </div>
+  </div>
+</template>
+<script setup>
+import {ref} from "vue";
+import Card from "@/components/Card.vue";
+
+const books = ref([
+  {
+    title: 'Метро 2033',
+    description: 'Постапокалиптический роман о выживании людей в московском метро после ядерной войны.',
+    genre: 'Постапокалипсис',
+    cover: 'metro2033',
+    adult: true,
+    rating: 4.7
+  },
+  {
+    title: 'Мастер и Маргарита',
+    description: 'Мистический роман о добре и зле, любви и дьяволе в Москве 1930-х годов.',
+    genre: 'Роман, мистика',
+    cover: 'master_margarete',
+    adult: false,
+    rating: 4.9
+  },
+  {
+    title: 'American Psycho',
+    description: 'Психологический триллер о двойной жизни успешного финансиста и серийного убийцы.',
+    genre: 'Триллер',
+    cover: 'american_psyho',
+    adult: true,
+    rating: 4.2
+  },
+  {
+    title: 'Игра престолов',
+    description: 'В мире Вестероса переплетаются политика, предательство, магия и судьбы множества героев. Книги известны своей реалистичностью, неожиданными поворотами и глубокой проработкой персонажей.',
+    genre: 'Фэнтези',
+    cover: 'game_of_thrones',
+    adult: true,
+    rating: 4.7
+  }
+])
+</script>
+
+
+<style scoped>
+ * {
+   box-sizing: border-box;
+ }
+
+ body {
+   background-color: rgb(228, 226, 223);
+ }
+
+ .container {
+   padding: 20px;
+   display: flex;
+   flex-wrap: wrap;
+   justify-content: center;
+   align-items: flex-start;
+   gap: 20px;
+ }
+</style>
---
+++ day_3/src/components/Card.vue
@@ -0,0 +1,136 @@
+<script setup>
+  import {computed} from "vue";
+
+  const { book } = defineProps({
+    book: {
+      type: Object,
+    }
+  })
+
+  const ratingPercent = computed(() => {
+    return `${(book.rating / 5) * 100}%`
+  })
+</script>
+
+<template>
+ <div class="card">
+   <div
+       class="cover"
+       :style="{ backgroundImage: `url(./assets/img/webp/${book.cover}.webp)` }"
+   >
+    <img class="adult" v-show="book.adult" src="/assets/img/png/adult18.png" alt="adult" />
+   </div>
+  <div class="content">
+    <h3 class="title">
+      {{ book.title }}
+    </h3>
+    <div class="genre">
+      {{ book.genre }}
+    </div>
+    <p class="description">
+      {{ book.description }}
+    </p>
+    <div class="rating">
+      <div class="stars-bg">★★★★★</div>
+      <div class="stars-fill" :style="{ width: ratingPercent }">★★★★★</div>
+    </div>
+  </div>
+ </div>
+</template>
+
+<style scoped>
+    .card {
+      width: 220px;
+      min-height: 330px;
+      padding: 16px;
+      border-radius: 16px;
+      background: #fff;
+      box-shadow: 0 10px 30px rgba(0,0,0,.08);
+      cursor: pointer;
+
+      .cover {
+        width: 100%;
+        height: 150px;
+        border-radius: 12px;
+        background-size: cover;
+        background-position: top;
+        margin-bottom: 12px;
+        transition: height 0.3s;
+        position: relative;
+
+        .adult {
+          width: 30px;
+          height: 30px;
+          position: absolute;
+          top: -10px;
+          right: -10px;
+        }
+      }
+
+      &:hover .cover{
+        transition: all 0.5s;
+        height: 300px;
+      }
+
+      .content {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        align-items: flex-start;
+
+        .title {
+          font-family: sans-serif;
+          font-size: 18px;
+          color: #212121;
+          margin: 0;
+        }
+
+        .description {
+          margin: 0;
+          margin-top: 10px;
+          font-size: 14px;
+          color: #757575;
+          font-family: 'roboto';
+          display: -webkit-box;
+          -webkit-line-clamp: 3;
+          -webkit-box-orient: vertical;
+          overflow: hidden;
+        }
+
+        .genre {
+          padding: 2px 4px;
+          margin-top: 10px;
+          display: inline-block;
+          font-size: 10px;
+          color: #2474f6;
+          border-radius: 12px;
+          font-family: sans-serif;
+          background: #769cea;
+          border: 1px solid #2474f6;
+
+        }
+
+        .rating {
+          position: relative;
+          display: inline-block;
+          font-size: 12px;
+          line-height: 1;
+          margin-top: 10px;
+
+          .stars-bg {
+            color: #ddd;
+          }
+
+          .stars-fill {
+            color: gold;
+            position: absolute;
+            top: 0;
+            left: 0;
+            white-space: nowrap;
+            overflow: hidden;
+            pointer-events: none;
+          }
+        }
+      }
+    }
+</style>
