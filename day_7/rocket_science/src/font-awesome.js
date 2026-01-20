885edaaa62f1a321748f08015758e2e28081569e
Eric <eric@Erics-MacBook-Air.local>
2026-01-20 20:02:54 +0300

    prepare rocket

---
+++ day_7/rocket_science/src/font-awesome.js
@@ -0,0 +1,4 @@
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faCheck, faEdit, faPlus, faStar, faStarHalfStroke, faTrash, faXmark} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faStar, faStarHalfStroke, faEdit, faTrash, faXmark, faCheck, faPlus)
